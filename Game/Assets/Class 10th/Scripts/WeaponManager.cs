6cbb795fd573416a99d1ff9e6bfecd078e4ad62b
Gangbin12 <rudqls2023@naver.com>
2025-04-30 19:47:00 +0900

    4.30

--- Game/Assets/Class 10th/Scripts/WeaponManager.cs
+++ Game/Assets/Class 10th/Scripts/WeaponManager.cs
@@ -1,21 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class WeaponManager : MonoBehaviour
 {
+    [SerializeField] int count = 0;
+
     [SerializeField] GameObject [] weapons;
-    [SerializeField] List<GameObject> weaponsList;
 
     [SerializeField] Transform parentPosition;
+
+    [SerializeField] List <GameObject> weaponsList;
     
     void Start()
     {
-        for (int i = 0; i < weapons.Length; i ++)
+        for (int i = 0; i < weapons.Length; i++)
         {
-            gameObject.SetActive(weapons, parentPosition);  // Instantiate(prefab, parentPosition);
+            GameObject clone = Instantiate(weapons[i], parentPosition);
+
+            clone.gameObject.SetActive(false);
+
+            weaponsList.Add(clone);
+        }
+
+        weaponsList[count] .transform .parent = parentPosition;
+    }
+
+    private void Update()
+    {
+        if(Input .GetKeyDown(KeyCode.Space))
+        {
+            Swap();
+        }
+        if(Input .GetMouseButtonDown(0))
+        {
+            Attack();
         }
     }
-   
+
+    public void Swap()
+    {
+        weaponsList[count].gameObject.SetActive(false);
+
+        count  = (count +1) % weaponsList.Count;
+
+        weaponsList[count].gameObject.SetActive(true);
+    }
+
+    public void Attack()
+    {
+        weaponsList[count].GetComponent<Weapon>().Attack();
+    }
+
 
 }
